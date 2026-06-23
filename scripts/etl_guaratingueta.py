90821d88029071f12305dda5e277ff0cd91686c4
luiza-ctrl <araujomarialuiza3301@gmail.com>
2026-06-23 16:17:20 +0000

    Update etl_guaratingueta.py

--- scripts/etl_guaratingueta.py
+++ scripts/etl_guaratingueta.py
@@ -4,3 +4,8 @@ import pandas as pd
 import sqlite3
 import requests
 import io
+
+# 1. EXTRAIR - API IBGE Guaratinguetá código 3518403
+print("Extraindo dados do IBGE...")
+url = "https://servicodados.ibge.gov.br/api/v3/agregados/6579/periodos/2000-2022/variaveis/9324?localidades=N6[3518403]"
+dados = requests.get(url).json()
